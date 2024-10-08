8caff15cdb454b24464ad86d836846a663898a79
AWhite-Code <Alexwhitehead432@gmail.com>
2024-10-08 01:13:36 +0100

    Wonky commit. Began setting up data library. Long way to go.

---
+++ YesChef.Data/Infrastructure/YesChefContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Yes_Chef.Data;
+
+namespace YesChef.Data
+{
+    public class YesChefContextFactory : IDesignTimeDbContextFactory<YesChefContext>
+    {
+        public YesChefContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddUserSecrets<YesChefContextFactory>(optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<YesChefContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection")
+                                   ?? "Server=(localdb)\\mssqllocaldb;Database=YesChef;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new YesChefContext(optionsBuilder.Options);
+        }
+    }
+}
