3ec85f15c4e3eb9771f35a94c2f4dcdf5efc596a
AWhite-Code <Alexwhitehead432@gmail.com>
2024-11-01 18:25:35 +0000

    Improper using, fixed.

--- YesChef.Data/Models/RecipeTag.cs
+++ YesChef.Data/Models/RecipeTag.cs
@@ -1,7 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Yes_Chef.Models.Interfaces;
+using Yes_Chef.Models;
 
 public class RecipeTag
 {
